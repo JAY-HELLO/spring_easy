96562d2d1f29fe162f6a7d98e9f218d0d01f7c4d
Double_J <double04j@gmail.com>
2023-03-05 15:57:58 +0900

    initial commit

---
+++ src/main/java/com/example/spring_easy/SpringEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEasyApplication.class, args);
+    }
+
+}
