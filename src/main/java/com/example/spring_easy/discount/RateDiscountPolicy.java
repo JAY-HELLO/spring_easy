f26a0132abb5250e3540ca772d79528b4d73cd5d
Double_J <double04j@gmail.com>
2023-03-12 14:14:14 +0900

    정률할인 코드 작성 및 test 코드 작성

---
+++ src/main/java/com/example/spring_easy/discount/RateDiscountPolicy.java
@@ -0,0 +1,27 @@
+package com.example.spring_easy.discount;
+
+import com.example.spring_easy.member.Grade;
+import com.example.spring_easy.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    /***
+     *
+     * @param member
+     * @param price
+     * @return
+     */
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100; // 이러한 계산 로직은 항상 의심해 보아야 한다. 즉 test 해보아야 한다는 의미.
+        }else{
+            return 0;
+        }
+    }
+
+    // 윈도우에서 ctrl shift t 를 통해서 바로 test 파일 생성 가능
+
+}
---
+++ src/test/java/com/example/spring_easy/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,42 @@
+package com.example.spring_easy.discount;
+
+import com.example.spring_easy.member.Grade;
+import com.example.spring_easy.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    /***
+     * 실패테스트
+     */
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void vip_x(){
+        //given
+        Member member = new Member(1L, "memberBasic", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0); // 1000 일 경우 EXPECTED = 1000 이지만 실제는 0 오류가 나온다
+
+    }
+}
