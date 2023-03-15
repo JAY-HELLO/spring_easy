ba014528bafabfaf1c82f8a71ba7576a012150c1
Double_J <double04j@gmail.com>
2023-03-15 19:35:24 +0900

    add spring container

--- src/main/java/com/example/spring_easy/MemberApp.java
+++ src/main/java/com/example/spring_easy/MemberApp.java
@@ -3,11 +3,19 @@ package com.example.spring_easy;
 import com.example.spring_easy.member.Grade;
 import com.example.spring_easy.member.Member;
 import com.example.spring_easy.member.MemberService;
+import com.example.spring_easy.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args){
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //spring container 에  bean 등록
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
