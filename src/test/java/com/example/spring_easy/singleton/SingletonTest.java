54ff0f8d25d1d91349ae69b90406e6e45d3c6377
Double_J <double04j@gmail.com>
2023-03-22 16:19:03 +0900

    싱글톤 패턴 test

--- src/test/java/com/example/spring_easy/singleton/SingletonTest.java
+++ src/test/java/com/example/spring_easy/singleton/SingletonTest.java
@@ -19,4 +19,20 @@ public class SingletonTest {
         //스프링 없는 순수학 di 컨테이너 appConfig는 요청을 할때 마다 객체를 새로 생성한다. -> 메모리 낭비가 심하다
         assertThat(ms1).isNotSameAs(ms2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체사용")
+    //같은객체 인스턴스가 반환되었음을 확인.
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("s s 1 = " + singletonService1);
+        System.out.println("s s 1 = " + singletonService2);
+        
+        assertThat(singletonService1).isSameAs(singletonService2);
+        
+        //same == 객체 인스턴스 비교
+        //equal == 값 비교
+    }
 }
