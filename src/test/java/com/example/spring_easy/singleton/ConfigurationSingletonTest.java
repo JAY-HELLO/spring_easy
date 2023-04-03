bdb4f0dabb2311799c48755ef1a32000f87fd5b6
Double_J <double04j@gmail.com>
2023-04-03 15:27:01 +0900

    Configuration annotation check

---
+++ src/test/java/com/example/spring_easy/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,37 @@
+package com.example.spring_easy.singleton;
+
+import com.example.spring_easy.AppConfig;
+import com.example.spring_easy.member.MemberRepository;
+import com.example.spring_easy.member.MemberServiceImpl;
+import com.example.spring_easy.order.Order;
+import com.example.spring_easy.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository",MemberRepository.class);
+
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = "+ memberRepository2);
+        System.out.println(" memberRepository = "+ memberRepository);
+    }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+    }
+}
